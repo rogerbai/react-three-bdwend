f3033411d2f57b8be4f852fc1065e41e2b0c172e
baizhanhua <baizhanhua@126.com>
2022-02-14 10:43:08 +0800

    Further change to model initial rotation.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
       <pointLight position={[-6, 6, 6]} />
       <Suspense fallback={null} >
         <Environment path="/cube" />
-        <PresentationControls speed={1.5} global zoom={1.5} polar={[-0.1, Math.PI / 2]} rotation={[0.6, -0.7, -0.3]}>
+        <PresentationControls speed={1.5} global zoom={1.5} polar={[-0.1, Math.PI / 2]} rotation={[0.7, -0.5, -0.1]}>
           <Suspense fallback={null} castshadow>
             <Model position={[0, -1.2,0]} scale={[5,5,5]} />
           </Suspense>
